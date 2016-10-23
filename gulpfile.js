9bbd277d3ba65a8a00e150738131c0ba320a7620
Pratik Dashore <pratikdashore@hotmail.com>
2016-10-23 19:29:52 +0530

    added js hint and jscs

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,28 @@
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
+var gulpUtil = require('gulp-util');
 
 gulp.task('hello-world', function() {
     console.log('hello gulp');
 });
+
+
+
+
+
+
+/////////////
+function log(msg) {
+    if (typeof msg === 'object') {
+        for (var item in msg) {
+            if (msg.hasOwnProperty(item)) {
+                gulpUtil.log(gulpUtil.colors.blue(msg[item]));
+            }
+        }
+    } else {
+        gulpUtil.log(gulpUtil.colors.blue(msg));
+    }
+    if (true) { console.log('hello'); }
+
+}
